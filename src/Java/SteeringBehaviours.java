0a0d49832a6419ce55b3d89b434b4cfb8d937cb6
dxcam1 <justinjoelward@gmail.com>
2014-06-25 18:26:44 -0500

    Class made static, Method(s) updated.
    forward vector is now calculated from Spatial rotation, Wander generates random unit vector. optimizations made to Avoid and syntax has been changed.

--- src/Java/SteeringBehaviours.java
+++ src/Java/SteeringBehaviours.java
@@ -1,59 +1,43 @@
-package mygame;
-
 import com.jme3.math.Vector3f;
 import com.jme3.math.Ray;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
-import com.simsilica.es.EntityComponent;
-import com.simsilica.es.EntityId;
 
-public class SteeringBehaviours implements EntityComponent{
-    EntityId id;
-    
-    public SteeringBehaviours(){
-        
-    }
-    
-    public SteeringBehaviours getValue(){
-        return this;
-    }
+public final static class SteeringBehaviours{
     
     public static final float Random(float min, float max){
         max = (float) (max * Math.random());
         return min + max;
     }
     
-    public Vector3f Seek(Spatial self, Spatial target){
+    public static Vector3f Seek(Spatial self, Spatial target){
         return (target.getWorldTranslation().subtract(self.getWorldTranslation()).normalize());
     }
     
-    public Vector3f Seek(Vector3f self, Vector3f target){
+    public static Vector3f Seek(Vector3f self, Vector3f target){
         return (target.subtract(self).normalize());
     }
     
-    public Vector3f Wander(Vector3f self, float r){
-        return new Vector3f(self.x + Random(0.0f,r), self.y + Random(0.0f,r), self.z + Random(0.0f,r));
+    public Vector3f Wander(Vector3f self){
+        return new Vector3f(Random(0.0f,1.0f),Random(0.0f,1.0f),Random(0.0f,1.0f));
     }
     
-    public Vector3f Flee(Spatial self, Spatial target){
+    public static Vector3f Flee(Spatial self, Spatial target){
         return self.getWorldTranslation().subtract(target.getWorldTranslation()).normalize();
     }
     
-    public Vector3f Flee(Vector3f self, Vector3f target){
+    public static Vector3f Flee(Vector3f self, Vector3f target){
         return self.subtract(target).normalize();
     }
     
-    public Vector3f Avoid(Spatial self, Spatial[] others, float maxDistance){
+    public static Vector3f Avoid(Spatial self, Spatial[] others, float maxDistance){
         Vector3f dir = Vector3f.ZERO;
-        Vector3f ahead = self.getWorldTranslation().add(Vector3f.UNIT_Z);
-        
+        Vector3f forward = self.getWorldTranslation().add(
+        self.getWorldTransform().getRotation().toRotationMatrix().mult(new Vector3f(1,1,1)).normalize());
+        Main.entityData.getComponent(id, Spatial)
         for(Spatial s : others)
-            if(s.getWorldTranslation().distance(ahead) > maxDistance/2)
-                ahead.subtract(s.getWorldTranslation());
+            if(s.getWorldTranslation().distanceSquared(self.getWorldTranslation()) > maxDistance/2)
+                forward.subtract(Vector3f.UNIT_XYZ.divide(s.getWorldTranslation()));
         return dir.normalize();
-    }
-    
-    public Vector3f Arrive(){
-        
     }
 }
